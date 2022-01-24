0f657c8062423dec98c79ebfc52cdf6161c5611b
Jason Telfer <jason@jasont.dev>
2022-01-24 21:23:03 +1000

    Basic setup of apollo-server with express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('Welcome to Node');
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world, my name is mutodo'
+  }
+};
+
+(async () => {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const app = express();
+
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
-const path = require("path");
-const NodemonPlugin = require("nodemon-webpack-plugin");
+const path = require('path');
+const NodemonPlugin = require('nodemon-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: "./src/index.js",
-  target: "node",
+  entry: './src/index.js',
+  target: 'node',
+  mode: 'none',
   resolve: {
-    extensions: [".js"],
+    extensions: ['.js']
   },
   output: {
-    filename: "server.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist')
   },
   plugins: [new NodemonPlugin()],
+  externals: [nodeExternals()]
 };
