199e7799498489bea97e76fda014509aefbecfc2
Jason Telfer <jason@jasont.dev>
2022-01-30 11:51:27 +1000

    Changed hmac.digest() encoding to 'hex'

--- src/password.js
+++ src/password.js
@@ -4,7 +4,7 @@ const hashPassword = (rawPassword, passwordSalt, passwordHashSecret) => {
   const hmac = createHmac('sha256', passwordHashSecret);
   hmac.update(rawPassword);
   hmac.update(passwordSalt);
-  return hmac.digest('utf8');
+  return hmac.digest('hex');
 };
 
 const createSalt = () => randomBytes(64).toString('base64');
